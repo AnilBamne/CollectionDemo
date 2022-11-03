21553fa8665c7615adfc9b6ea1ed9bc6675b23cb
AnilBamne <99521630+AnilBamne@users.noreply.github.com>
2022-11-03 16:19:50 +0530

    DictionaryInCollection

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace CollectionDemo
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Welcome to c# collection!");
+            //ListDemo list1 = new ListDemo();
+            //list1.List1();
+            //ListDemo.doListDemo();
+
+
+            //CollectionSample.doStackDemo();
             DictionaryInCollection.DictionaryDemo();
+            //CollectionSample.doQueueDemo();
+            // CollectionSample.doSetDemo();
         }
     }
 }
