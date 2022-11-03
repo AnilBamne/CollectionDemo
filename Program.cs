9d6bba9b9e157b7a8e535223d16c48db1ffbb2ad
AnilBamne <anilbamne20@gmail.com>
2022-11-03 16:08:40 +0530

    QueueIncollections

--- Program.cs
+++ Program.cs
@@ -6,33 +6,7 @@ namespace CollectionDemo
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            Console.WriteLine("Welcome to c# collection!");
-            ListDemo list1 = new ListDemo();
-            list1.List1();
-            ListDemo.doListDemo();
-
-=======
-            //Console.WriteLine("Welcome to c# collection!");
-            //ListDemo list1 = new ListDemo();
-            //list1.List1();
-            //ListDemo.doListDemo();
-
-            SetDemo.doSetDemo();
->>>>>>> Set
-
-            //CollectionSample.doStackDemo();
-            //CollectionSample.doDictionaryDemo();
-            //CollectionSample.doQueueDemo();
-<<<<<<< HEAD
-            // CollectionSample.doSetDemo();
-=======
-
->>>>>>> Set
-=======
-            StackDemo.Stack();
->>>>>>> Stack
+            QueueInCollection.QueueDemo();
         }
     }
 }
---
+++ QueueInCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    public class QueueInCollection
+    {
+        public static void QueueDemo()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Amit Sharma");
+
+            queue.Enqueue("Vijay Raj");
+            queue.Enqueue("Jaishankar");
+            queue.Enqueue("Raj");
+            Console.WriteLine("Head:" + queue.Peek());
+            Console.WriteLine("\nIterating the queue elements: ");
+            foreach (var element in queue)
+            {
+                Console.WriteLine(element);
+            }
+            string dequeue = queue.Dequeue();
+            Console.WriteLine("Dequeue element: " + dequeue);
+            Console.WriteLine("\nIterating the queue elements after dequeu one element:");
+            Queue<string>.Enumerator enumerator = queue.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
