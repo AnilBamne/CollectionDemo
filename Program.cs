63beb9a9ac3e6f2b91c052102b8f3beab59afcfe
AnilBamne <anilbamne20@gmail.com>
2022-11-03 14:50:42 +0530

    WelcomeToCollectionInC#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to c# collection!");
+            //ListDemo list1 = new ListDemo();
+            //list1.List1();
+
+            //CollectionSample.doListDemo();
+            //CollectionSample.doStackDemo();
+            //CollectionSample.doDictionaryDemo();
+            //CollectionSample.doQueueDemo();
+           // CollectionSample.doSetDemo();
+        }
+    }
+}
